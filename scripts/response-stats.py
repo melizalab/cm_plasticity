165ad6a5f71f725675a151c5165e962b3be03a00
Dan Meliza <dan@meliza.org>
2022-09-29 12:18:18 -0400

    initial work on stage 2

---
+++ scripts/response-stats.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+"""Compute electrophysiology stats from epoch pprox files """
+import datetime
+import logging
+import json
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from core import setup_log
+
+log = logging.getLogger()
+__version__ = "20220928"
+
+
+def load_epoch(path):
+    log.debug(" - reading %s", path)
+    with open(path, "rt") as fp:
+        data = json.load(fp)
+        epoch = pd.json_normalize(
+            data,
+            "pprox",
+            ["cell", "epoch", "bird", "sire", ["stats", "tau"], ["stats", "Cm"]],
+        )
+        return (
+            epoch.rename(columns={"index": "sweep", "stats.tau": "tau", "stats.Cm": "Cm"})
+            .set_index(["cell", "epoch", "sweep"])
+            .drop(columns=["offset", "interval"])
+        )
+
+
+def sweep_firing_stats(sweep):
+    try:
+        step = pd.Interval(*sweep["stimulus.interval"])
+        spikes = [e for e in sweep.events if e in step]
+        rate = len(spikes) / step.length
+        if len(spikes) == 0:
+            duration = np.nan
+        elif len(spikes) == 1:
+            duration = (
+                sweep["first_spike.width"] + sweep["first_spike.trough_t"]
+            ) / 1000.0
+        else:
+            duration = sweep.events[-1] - sweep.events[0]
+    except TypeError:
+        rate = duration = np.nan
+    return pd.Series(
+        [sweep["stimulus.I"], rate, duration, sweep.Rs, sweep.Rm, sweep.temperature],
+        index=["current", "firing_rate", "firing_duration", "Rs", "Rm", "temperature"],
+    )
+
+
+def epoch_firing_stats(sweeps):
+    # find sweeps with spikes
+    (idx,) = (sweeps.firing_rate > 0).to_numpy().nonzero()
+    df = sweeps.iloc[idx[0] - 1 :]
+    # rheobase: midpoint between current levels that evoke firing
+    I_0 = (df.current[1] + df.current[0]) / 2
+    # f-I slope: average of the slopes (simpler and more stable than linear regression)
+    slope = np.mean(np.diff(df.firing_rate) / np.diff(df.current))
+    return pd.Series(
+        [
+            sweeps.firing_duration.max(),
+            sweeps.firing_duration.mean(),
+            sweeps.firing_rate.max(),
+            I_0,
+            slope,
+            sweeps.Rs.mean(),
+            sweeps.Rm.mean(),
+            sweeps.temperature.mean(),
+        ],
+        index=[
+            "duration_max",
+            "duration_mean",
+            "rate_max",
+            "rheobase",
+            "fI_slope",
+            "Rs",
+            "Rm",
+            "T",
+        ],
+    )
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument(
+        "--debug", help="show verbose log messages", action="store_true"
+    )
+    parser.add_argument(
+        "epochs", type=Path, nargs="+", help="epoch pprox files to process"
+    )
+    args = parser.parse_args()
+    setup_log(log, args.debug)
+    log.info("- date: %s", datetime.datetime.now())
+    log.info("- version: %s", __version__)
+
+    sweeps = pd.concat([load_epoch(path) for path in args.epochs])
+    cells = (
+        sweeps.reset_index()[["cell", "bird", "sire"]]
+        .drop_duplicates()
+        .set_index("cell")
+    )
+    epochs = (
+        sweeps.reset_index()[["cell","epoch","tau","Cm"]]
+        .drop_duplicates()
+        .set_index(["cell", "epoch"])
+    )
+    log.info("- computing sweep-level statistics")
+    sweep_stats = sweeps.apply(sweep_firing_stats, axis=1)
+    log.info("- computing epoch-level statistics")
+    epoch_stats = (
+        sweep_stats
+        .groupby(["cell", "epoch"])
+        .apply(epoch_firing_stats)
+        .join(epochs)
+    )
+    print(epoch_stats)
