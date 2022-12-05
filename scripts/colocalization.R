daba7828e59e77189a52ba0be5d650f7b62ac3d5
Dan Meliza <dan@meliza.org>
2022-12-05 10:55:35 -0500

    use cdm (blinded) classification

--- scripts/colocalization.R
+++ scripts/colocalization.R
@@ -8,11 +8,41 @@ my.theme <- egg::theme_article() + theme(legend.position="none",
                                          axis.text=element_text(size=6))
 
 coloc_epochs = read_csv("inputs/colocalization_epochs.csv")
-biocytin_cells = (
+biocytin_cells_yao = (
      read_csv("inputs/biocytin_cells.csv")
+     ## %>% filter(!is.na(cell), !is.na(kv11))
+)
+biocytin_cells = (
+     read_csv("inputs/biocytin_cells_dan.csv")
      %>% filter(!is.na(cell), !is.na(kv11))
 )
 epoch_stats = read_csv("build/epoch_stats.csv") %>% semi_join(coloc_epochs, by=c("cell", "epoch"))
+
+## average epochs
+## filter out narrow-spiking cells       
+cell_stats = (
+     group_by(epoch_stats, cell) 
+     %>% summarize(duration_mean=mean(duration_mean, na.rm=T),
+                  slope_mean=mean(slope, na.rm=T),
+	          spike_width=median(spike_width, na.rm=T),
+	          temperature=median(temperature, na.rm=T))
+      %>% filter(spike_width > 0.9)
+      %>% inner_join(biocytin_cells, by="cell")
+)
+
+## manual classification:
+p1 <- (
+   cell_stats
+   %>% ggplot(aes(kv11, duration_mean))
+   + geom_jitter(width=0.1, fill="white", shape=21, size=2)
+   + ylab("Duration (s)")
+   + xlab("Kv1.1")
+)
+
+## simple stats:
+wilcox.test(duration_mean ~ kv11, cell_stats)
+
+## NOT USED: automated pipeline
 
 ## combine counts across sections in kv11 ihc data
 kv11_stats = (
@@ -28,31 +58,9 @@ kv11_stats = (
      %>% mutate(density=puncta/volume * 1000)
 )
 
-## average epochs
-## filter out narrow-spiking cells       
-cell_stats = (
-     group_by(epoch_stats, cell) 
-     %>% summarize(duration_mean=mean(duration_mean, na.rm=T),
-                  slope_mean=mean(slope, na.rm=T),
-	          spike_width=median(spike_width, na.rm=T),
-	          temperature=median(temperature, na.rm=T))
-      %>% filter(spike_width > 0.9)
-      %>% inner_join(biocytin_cells, by="cell")
-      %>% left_join(kv11_stats, by="image")
-)
-
-## manual classification:
-p1 <- (
-   cell_stats
-   %>% ggplot(aes(kv11, duration_mean))
-   + geom_jitter(width=0.1, fill="white", shape=21, size=2)
-   + ylab("Duration (s)")
-   + xlab("Kv1.1")
-)
-
-## automated pipeline
 p2 <- (
    cell_stats
+   %>% left_join(kv11_stats, by="image")
    %>% ggplot(aes(density, duration_mean))
    + geom_point(fill="white", shape=21, size=2)
    + ylab("Duration (s)")
@@ -61,5 +69,3 @@ p2 <- (
 
 ## TODO make this look pretty
 
-## simple stats:
-wilcox.test(duration_mean ~ kv11, cell_stats)
